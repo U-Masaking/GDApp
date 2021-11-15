11ee7f71bfc73de4a7ae5b0375577bc1f5eb563c
U-Masaking <uchi119_itl@outlook.jp>
2021-11-15 13:31:49 +0900

    created mypage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    #devieseコントローラーのアクション実行前にパラメーターを確認する
     before_action :configure_permitted_parameters, if: :devise_controller?
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
-
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    if user_signed_in?
+      redirect_to users_index_path
+    else
+      redirect_to root_path
+    end
   end
 end
